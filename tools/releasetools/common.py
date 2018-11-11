e6906e860058c0776453138903bc3b334b6f0f6f
Nicolas Dhouailly <ndhouailly@gmail.com>
2018-11-11 13:56:31 +0100

    Display progress of system & vendor images when flashing
    * Previously system image was displayed twice instead of displaying system and vendor images progress
    
    Change-Id: Ic4e427cb6a34f93ab48c8e2a92942ac6b341d60b
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -3478,7 +3478,10 @@ class BlockDifference(object):
     if not self.src:
       # write the output unconditionally
       script.Print(" ")
-      script.Print("Flashing aosPB System files...")
+      script.Print("Flashing aosPB %s files..." % (self.partition,))
+    else:
+      script.Print(" ")
+      script.Print("Flashing aosPB %s files after verification." % (self.partition,))
 
     if progress:
       script.ShowProgress(progress, 0)
@@ -3578,6 +3581,8 @@ class BlockDifference(object):
 
   def WritePostInstallVerifyScript(self, script):
     partition = self.partition
+    script.Print(" ")
+    script.Print('Verifying aosPB %s files...' % (partition,))
     # Unlike pre-install verification, clobbered_blocks should not be ignored.
     ranges = self.tgt.care_map
     ranges_str = ranges.to_string_raw()
@@ -3595,7 +3600,11 @@ class BlockDifference(object):
               self.device, ranges_str,
               self._HashZeroBlocks(self.tgt.extended.size())))
       script.Print(" ")
-      script.Print("Verified aosPB System files...")
+      script.Print('Verified aosPB %s files.' % (partition,))
+      if partition == "system":
+        code = ErrorCode.SYSTEM_NONZERO_CONTENTS
+      else:
+        code = ErrorCode.VENDOR_NONZERO_CONTENTS
       script.AppendExtra(
           'else\n'
           '  abort("E%d: %s partition has unexpected non-zero contents after '
@@ -3603,7 +3612,7 @@ class BlockDifference(object):
           'endif;' % (code, partition))
     else:
       script.Print(" ")
-      script.Print("Verified aosPB System files...")
+      script.Print('Verified aosPB %s files.' % (partition,))
 
     if partition == "system":
       code = ErrorCode.SYSTEM_UNEXPECTED_CONTENTS
