DerpFest-AOSP/build

releasetools: Correct flashing and verified text
Change-Id: I15b0e034094c0cf4b60a86f5f26fc8d77ab50e54
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

tools/releasetools/common.py
@@ -3229,10 +3229,10 @@ class BlockDifference(object):
     if not self.src:
       # write the output unconditionally
       script.Print(" ")
-      script.Print("Flashing DerpFest %s files..." % (self.partition,))
+      script.Print("Flashing DerpFest %s partition..." % (self.partition,))
     else:
       script.Print(" ")
-      script.Print("Flashing DerpFest %s files after verification." % (self.partition,))
+      script.Print("Flashing DerpFest %s partition after verification." % (self.partition,))
 
     if progress:
       script.ShowProgress(progress, 0)
@@ -3351,7 +3351,7 @@ class BlockDifference(object):
               self.device, ranges_str,
               self._HashZeroBlocks(self.tgt.extended.size())))
       script.Print(" ")
-      script.Print('Verified DerpFest %s files.' % (partition,))
+      script.Print('Verified DerpFest %s partition.' % (partition,))
       if partition == "system":
         code = ErrorCode.SYSTEM_NONZERO_CONTENTS
       else:

aospb-staging/build_make

tools/releasetools/common.py
@@ -3478,10 +3478,10 @@ class BlockDifference(object):
     if not self.src:
       # write the output unconditionally
       script.Print(" ")
-      script.Print("Flashing aosPB %s files..." % (self.partition,))
+      script.Print("Flashing aosPB %s partition..." % (self.partition,))
     else:
       script.Print(" ")
-      script.Print("Flashing aosPB %s files after verification." % (self.partition,))
+      script.Print("Flashing aosPB %s partition after verification." % (self.partition,))
 
     if progress:
       script.ShowProgress(progress, 0)
@@ -3600,7 +3600,7 @@ class BlockDifference(object):
               self.device, ranges_str,
               self._HashZeroBlocks(self.tgt.extended.size())))
       script.Print(" ")
-      script.Print('Verified aosPB %s files.' % (partition,))
+      script.Print('Verified aosPB %s partition.' % (partition,))
       if partition == "system":
         code = ErrorCode.SYSTEM_NONZERO_CONTENTS
       else: